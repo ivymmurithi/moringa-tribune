86bd047d283b36a1ff85c624f3f532b3a78be8e7
ivymmurithi <ivymurithi@gmail.com>
2022-02-28 20:38:42 +0300

    get values from forms after posting

--- news/views.py
+++ news/views.py
@@ -1,9 +1,9 @@
 from django.forms import ValidationError
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404,HttpResponse, HttpResponseRedirect
 import datetime as dt
 from django.shortcuts import render,redirect
-from .models import Article
+from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 
 # Create your views here.
@@ -30,7 +30,11 @@ def news_today(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name, email = email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date":date,"news":news, "letterForm":form})
