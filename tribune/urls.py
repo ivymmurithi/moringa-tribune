2d29c634f27973388b0600951a9e638a972fe18c
ivymmurithi <ivymurithi@gmail.com>
2022-03-01 13:58:55 +0300

    add registration url

--- tribune/urls.py
+++ tribune/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls'))
+    path('', include('news.urls')),
+    path('accounts/', include('registration.backends.simple.urls'))
 ]
