814450ee456a80271a012bc0989d33355c804628
ivymmurithi <ivymurithi@gmail.com>
2022-03-09 13:57:52 +0300

    add serializer file and modelserializer

---
+++ news/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import MoringaMerch
+
+class MerchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoringaMerch
+        fields = ('name','description','price')
